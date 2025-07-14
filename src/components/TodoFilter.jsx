e09d5a2f8b23e01b277c3e9285df9c0ee684f02d
user.email <mrdilmurat777@gmail.com>
2025-07-14 15:56:42 +0600

    feat: add read, delete, update, filter

--- src/components/TodoFilter.jsx
+++ src/components/TodoFilter.jsx
@@ -1,14 +1,42 @@
+import { CheckCircle, Clock, List } from 'lucide-react';
 
-
-const TodoFilter = () => {
+const TodoFilter = ({ filter, stats, handleFilterChange }) => {
+  const filters = [
+    {
+      key: 'all',
+      label: 'All',
+      icon: List,
+      count: stats.total,
+    },
+    {
+      key: 'active',
+      label: 'Active',
+      icon: Clock,
+      count: stats.active,
+    },
+    {
+      key: 'completed',
+      label: 'Completed',
+      icon: CheckCircle,
+      count: stats.completed,
+    },
+  ];
   return (
-	  <div className="flex items-center justify-center">
-		  <div className="inline-flex bg-gray-200">
-			  {/* I will add logic */}
-		  </div>
-	  <h2>TodoFilter</h2>
-	</div>
-  )
-}
+    <div className="flex items-center justify-center">
+      <div className="inline-flex bg-gray-200 rounded-lg p-1">
+        {/* I will add logic */}
+        {filters.map(({ key, label, icon: Icon, count }) => {
+          return (
+            <button key={key} className={`flex items-center gap-2 px-2 py-2 rounded-md text-sm font-medium transition-all duration-200 ${filter === key ? 'bg-white text-gray-800 shadow-md' : 'text-gray-700 hover:text-gray-800 hover:bg-gray-300'}`} onClick={() => handleFilterChange(key)}>
+              <Icon size={16} />
+              <span>{label}</span>
+              <span>{count}</span>
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default TodoFilter
+export default TodoFilter;
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,22 +1,74 @@
-import { Check, X } from 'lucide-react'
-import React from 'react'
+import { Check, X } from 'lucide-react';
+import React, { useRef, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo, setIsAddingTodo } from '../store/todoSlice';
 
-const TodoForm = () => {
+const TodoForm = ({ OnSubmit, OnCancel, initialValue = '', placeholder = 'Add a new Todo...' }) => {
+  const dispatch = useDispatch();
+  const [text, setText] = useState(initialValue);
+	const inputRef = useRef(null);
+	
+	console.log(text);
+	
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const trimmedText = text.trim()
+		if (trimmedText) {
+			if (OnSubmit) {
+				//for editing
+				OnSubmit(trimmedText)
+			} else {
+				dispatch(addTodo(trimmedText))
+			}
+			setText('')
+		}
+	}
+
+	const handleCancel = () => {
+		if (OnCancel) {
+			onCancel()
+		} else {
+			dispatch(setIsAddingTodo(false))
+		}
+		setText('')
+	}
+
+	const handleKeyDown = (e) => {
+		if (e.key === "Escape") {
+			handleCancel()
+		}
+	}
+
   return (
-	  <div className='flex items-center gap-3'>
-		  <div className="flex-1">
-			  <input type="text" className='w-full px-4 py-3  border border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-800 focus:border-transparent transition-all duration-200 bg-white/90 backdrop-blur-sm text-gray-800 placeholder:gray-600' maxLength={500} />
-		  </div>
-		  <div className="flex items-center gap-2">
-			  <button type='submit' className='flex items-center justify-center w-10 h-10 bg-green-600 hover:bg-green-700 disabled:bg-gray-400 disabled:cursor-not-allowed text-white rounded-lg transition-colors duration-200' title="Save Todo">
-				  <Check size={16} />
-			  </button>
-			  <button type='submit' className='flex items-center justify-center w-10 h-10 bg-gray-600 hover:bg-gray-700 disabled:bg-gray-400 disabled:cursor-not-allowed text-white rounded-lg transition-colors duration-200' title="Save Todo">
-				  <X size={16} />
-			  </button>
-	  </div>
-	</div>
-  )
-}
+    <form className="flex items-center gap-3" onSubmit={handleSubmit}>
+      <div className="flex-1">
+        <input
+          ref={inputRef}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+				  placeholder={placeholder}
+				  onKeyDown={handleKeyDown}
+          className="w-full px-4 py-3  border border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-800 focus:border-transparent transition-all duration-200 bg-white/90 backdrop-blur-sm text-gray-800 placeholder:gray-600"
+          maxLength={500}
+        />
+      </div>
+      <div className="flex items-center gap-2">
+        <button
+          type="submit"
+          className="flex items-center justify-center w-10 h-10 bg-green-600 hover:bg-green-700 disabled:bg-gray-400 disabled:cursor-not-allowed text-white rounded-lg transition-colors duration-200"
+          title="Save Todo">
+          <Check size={16} />
+        </button>
+        <button
+          type="submit"
+          className="flex items-center justify-center w-10 h-10 bg-gray-600 hover:bg-gray-700 disabled:bg-gray-400 disabled:cursor-not-allowed text-white rounded-lg transition-colors duration-200"
+          title="Save Todo" onClick={handleCancel}>
+          <X size={16} />
+        </button>
+      </div>
+    </form>
+  );
+};
 
-export default TodoForm
+export default TodoForm;
